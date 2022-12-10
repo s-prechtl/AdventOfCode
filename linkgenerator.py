ddb9cc5bf9209cc6ec0b8b36a0db703b6b307cea
s-prechtl <stefan@tague.at>
2022-12-10 17:41:18 +0100

    quick link generator, for the reddit solution posts

---
+++ linkgenerator.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    # https://github.com/s-prechtl/AdventOfCode/tree/master/2022/Day10
+    user = "s-prechtl"
+    repo = "AdventOfCode"
+    branch = "master"
+    year = 2022
+    day = input("Day: ")
+    print(f"https://github.com/{user}/{repo}/tree/{branch}/{year}/Day{day}")
