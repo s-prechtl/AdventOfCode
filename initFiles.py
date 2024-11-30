2cee4e9b0467c7e321079b5caeaa3bd57f363a07
s-prechtl <stefan@tague.at>
2024-11-30 23:54:49 +0100

    feat: change it to fit c better

--- initFiles.py
+++ initFiles.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     # create folders for the days and the py files
     for i in range(1, 26):
         if not os.path.exists(year + '/Day' + str(i)):
-            os.makedirs(year + '/Day' + str(i))
-            f = open(year + '/Day' + str(i) + '/Day' + str(i) + '.' + filetype, 'w')
+            os.makedirs(year + '/Day' + str(i) + '/src')
+            f = open(year + '/Day' + str(i) + '/src/main.' + filetype, 'w')
             f.close()
             f = open(year + '/Day' + str(i) + '/input.txt', 'w')
             f.close()
